cedae4ae3f6b7a06f07a015c459a61132a0dfe59
Danielji06 <112855656+Danielji06@users.noreply.github.com>
2023-05-09 12:44:58 +0700

    Add files via upload

---
+++ dict05.py
@@ -0,0 +1,20 @@
+data = dict()
+while True:
+    angka = int(input("Masukkan angka [negatif untuk berhenti]: "))
+    # jika negatif maka program selesai
+    if angka < 0:
+        break
+    else:
+        # Kelompokkan angka ganjil dan genap
+        # 1. ganjil atau genap
+        if angka % 2 == 0: # genap
+            if data.get("genap") != None:
+                data["genap"].append(angka)
+            else:
+                data["genap"] = [angka]
+        else:
+            if data.get("ganjil") != None:
+                data["ganjil"].append(angka)
+            else:
+                data["ganjil"] = [angka]
+print(data)
